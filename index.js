89cfc1d7da24580b38156e1387053e81dbe30f12
adam <adam@opalecky.cz>
2021-01-22 14:58:24 +0100

    shrugs well

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
-const http = require('http');
-const port = 3000;
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = 80;
+
+
+app.listen(port, ()=>{
+    console.log('Listening on port', port);
+})
+
+app.use(express.static(path.join(__dirname, 'public')));
---
+++ readme.md
@@ -0,0 +1,1 @@
+#start with `forever -w index.js`
